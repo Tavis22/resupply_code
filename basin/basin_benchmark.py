76d2d74ffd95dbaa79826a6e2252cf5bf481fb7c
Tavis <tavobenne@hotmail.com>
2021-01-17 15:21:57 +0800

    basin_benchmark

---
+++ basin/basin_benchmark.py
@@ -0,0 +1,44 @@
+import quop_mpi as qu
+import numpy as np
+from mpi4py import MPI
+from numpy import loadtxt
+
+trials = 3
+max_p = 8
+
+q_global = loadtxt('8nodeQualityVector.csv', delimiter=',')
+
+n_soln = len(q_global)
+
+def local_qualities(N, local_i, local_i_offset, seed = None):
+    return q_global[local_i_offset:local_i_offset + local_i]
+
+comm = MPI.COMM_WORLD
+
+rng = np.random.RandomState(1)
+
+def x0(p,seed):
+    return rng.uniform(low = 0, high = 2*np.pi, size = 2 * p)
+
+qwoa = qu.MPI.qwoa(n_soln, comm, qubits = False)
+
+qwoa.set_initial_state(name="equal")
+
+qwoa.set_optimiser('scipy_basinhopping',
+                   {'minimizer_kwargs':{'method':'Nelder-Mead','tol':1e-3,'options':{'maxfev':5000,'maxiter':5000}}},
+                   ['fun']
+)
+
+qwoa.log_results("qwoa_complete_equal", "equal", action = "a")
+
+qwoa.plan()
+
+qwoa.benchmark(
+    range(1, max_p + 1),
+    trials,
+    param_func = x0,
+    qual_func = local_qualities,
+    param_persist = False,
+    filename = "qwoa_complete_equal",
+    label = "qwoa")
+qwoa.destroy_plan()
