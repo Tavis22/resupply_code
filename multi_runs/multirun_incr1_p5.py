03445af5db84dbb67a5abac67af65b4c1c96c611
Tavis <tavobenne@hotmail.com>
2021-01-22 12:49:09 +0800

    multirun increments

---
+++ multi_runs/multirun_incr1_p5.py
@@ -0,0 +1,71 @@
+import quop_mpi as qu
+import numpy as np
+from mpi4py import MPI
+from numpy import loadtxt
+import pandas as pd
+import h5py
+
+
+trials = 5
+p = 5
+runs = 10
+incr = 1
+
+q_global = loadtxt('8nodeQualityVector.csv', delimiter=',')
+
+n_soln = len(q_global)
+
+def local_qualities(N, local_i, local_i_offset, seed = None):
+    return q_global[local_i_offset:local_i_offset + local_i]/370
+
+comm = MPI.COMM_WORLD
+
+rng = np.random.RandomState(1)
+
+def x0(p,seed):
+    return rng.uniform(low = 0, high = 2*np.pi, size = 2 * p)
+
+qwoa = qu.MPI.qwoa(n_soln, comm, qubits = False)
+
+qwoa.set_initial_state(name="equal")
+
+qwoa.log_results("multirun_p"+str(p) + "_incr" + str(incr), "equal", action = "a")
+
+qwoa.plan()
+
+"""initialise"""
+qwoa.benchmark(
+        range(p, p + 1),
+        trials,
+        param_func = x0,
+        qual_func = local_qualities,
+        param_persist = False,
+        filename = "multirun_p" + str(p) + "_incr" + str(incr) + "_initial",
+        label = "qwoa")
+
+log = pd.read_csv('multirun_p' + str(p) + "_incr" + str(incr) + '.csv')
+expectation_values = log.fun 
+label = 'qwoa_' + str(p) + '_' + str(pd.Series.idxmin(expectation_values) + 1)
+data = h5py.File("multirun_p" + str(p) + "_incr" + str(incr) + '_initial.h5','r')
+nextstate = np.array(data[label]["final_state"]).view(np.complex128)
+qwoa.set_initial_state(state=nextstate)
+
+for i in range(runs):
+
+    qwoa.benchmark(
+        range(p+i, p+i+1),
+        trials,
+        param_func = x0,
+        qual_func = local_qualities,
+        param_persist = False,
+        filename = "multirun_p" + str(p) + "_incr" + str(incr)+ "_run" + str(i + 1),
+        label = "qwoa")
+    
+    log = pd.read_csv('multirun_p' + str(p) + "_incr" + str(incr) + '.csv')
+    expectation_values = log.fun 
+    label = 'qwoa_' + str(p+i) + '_' + str(pd.Series.idxmin(expectation_values[(i+1)*trials:(i+2)*trials])-(i+1)*trials + 1)
+    data = h5py.File("multirun_p" + str(p) + "_incr" + str(incr) + "_run" + str(i + 1) + '.h5','r')
+    nextstate = np.array(data[label]["final_state"]).view(np.complex128)
+    qwoa.set_initial_state(state=nextstate)
+    
+qwoa.destroy_plan()
---
+++ multi_runs/process_multirun_incr1_p5.py
@@ -0,0 +1,48 @@
+import h5py
+import numpy as np
+import pandas as pd
+
+trials = 5
+p = 5
+runs = 10
+incr = 1
+
+log = pd.read_csv('multirun_p' + str(p) + "_incr" + str(incr) + '.csv')
+expectation_values = log.fun 
+labels = []
+labels.append('qwoa_' + str(p) + '_' + str(pd.Series.idxmin(expectation_values[0:p]) + 1))
+for i in range(runs):
+    labels.append('qwoa_' + str(p+i) + '_' + str(pd.Series.idxmin(expectation_values[(i+1)*trials:(i+2)*trials])-(i+1)*trials + 1))
+
+data = h5py.File("multirun_p" + str(p) + "_incr" + str(incr) + '_initial.h5','r')
+quals = np.array(data[labels[0]]["qualities"]).view(np.float64)
+unique_quals = sorted(set(quals))
+number_unique_quals = len(unique_quals)
+data_out = {}
+run_labels = ['Initial_p' + str(p)] + ['p = ' + str(p+i) for i in range(runs)]
+
+qual_dict = {i : 0 for i in unique_quals}
+state = np.array(data[labels[0]]["final_state"]).view(np.complex128)
+probs = np.abs(state)**2
+for i in range(len(probs)):
+    qual_dict[quals[i]] += probs[i]
+distribution = []
+for i in qual_dict:
+    distribution.append(qual_dict[i])
+data_out[run_labels[0]] = distribution
+
+for j in range(runs):
+    data = h5py.File("multirun_p" + str(p) + "_incr" + str(incr) + "_run" + str(j + 1) + '.h5','r')
+    qual_dict = {i : 0 for i in unique_quals}
+    state = np.array(data[labels[j+1]]["final_state"]).view(np.complex128)
+    probs = np.abs(state)**2
+    for i in range(len(probs)):
+        qual_dict[quals[i]] += probs[i]
+    distribution = []
+    for i in qual_dict:
+        distribution.append(qual_dict[i])
+    data_out[run_labels[j+1]] = distribution
+
+output = pd.DataFrame(data_out, columns = run_labels, index = unique_quals)
+
+output.to_csv (r'multirun_p' + str(p) + "_incr" + str(incr) + '_qual_distributions.csv', index = True, header=True)
